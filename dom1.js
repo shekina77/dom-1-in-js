ab1b07b6d463cd1ff8e6f0de29353cdc87a682ec
unknown <shekinadhanunjay@gmail.com>
2024-11-14 23:08:02 +0530

    dom 1 in js

---
+++ dom1.js
@@ -0,0 +1,26 @@
+
+let d=document.getElementById("p")
+let k=document.getElementById("btn")
+
+
+function b(){
+    k.textContent="this is button"
+    k.style.fontSize="60px"
+    k.style.backgroundColor="pink"
+    k.style.borderRadius="50%"
+    
+}
+
+
+var t=document.getElementById("button");
+var a=document.getElementById("para")
+function z(){
+    a.style.borderRadius="30px"
+    a.style.display="block"
+    a.innerHTML=` <img src="https://png.pngtree.com/thumb_back/fh260/background/20230617/pngtree-little-girl-baby-girl-hd-wallpapers-indian-baby-image_2948600.jpg" style="border-radius:30px"/>
+    <img src="https://static.vecteezy.com/system/resources/thumbnails/035/583/695/small_2x/ai-generated-cute-little-girl-with-blue-eyes-in-a-white-shawl-adorable-little-baby-portrait-cute-baby-girl-indoor-ai-generated-free-photo.jpg" style="border-radius:100px" />
+    <img src="https://www.shutterstock.com/image-photo/pretty-girl-russian-model-flowers-600nw-1314777902.jpg" style="border-radius:50%"/> `
+   
+    
+
+}
